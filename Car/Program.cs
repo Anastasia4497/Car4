c33505ba22376386f31ed070fc8b3e8d19d3552b
Анастасия Боговид <anastaciabogovid@yandex.ru>
2022-12-18 21:59:02 +0300

    Car4

--- Car/Program.cs
+++ Car/Program.cs
@@ -2,9 +2,81 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<ICar> cars = new List<ICar>();
+            foreach (var car in cars)
+            {
+                car.Start();
+            }
+            foreach (var car in cars)
+            for (int i = 0; i < 4; i++)
+            {
+         
+                    car.ChangeGear();
+                }
+                foreach (var car in cars)
+                {
+                    car.Stop();
+                }
+            }
+        }
+                     public interface ICar
+        {
+            void Start();
+            void Stop();
+            void ChangeGear();
+            decimal CurrentSpeed { get; }
+            public class Melkus : ICar
+            {
+                private decimal _currentSpeed = 0m;
+                public decimal CurrentSpeed => _currentSpeed;
+                public void ChangeGear()
+                {
+                    _currentSpeed += 20;
+
+                }
+                public void Start()
+                {
+                    Console.WriteLine("Wroom");
+                }
+                public void Stop()
+                {
+                    Console.WriteLine("Bzz");
+                    _currentSpeed = 0;
+                }
+
+                public class Maserati : ICar
+                {
+                    private decimal _currentSpeed = 0m;
+                    public decimal CurrentSpeed => _currentSpeed;
+                    public void ChangeGear()
+                    {
+                        _currentSpeed += 90;
+
+                    }
+                    public void Start()
+                    {
+                        Console.WriteLine("Wroom");
+                    }
+                    public void Stop()
+                    {
+                        Console.WriteLine("Bzz");
+                        _currentSpeed = 0;
+                        {
+
+                        }
+
+
+
+                    }
+                }
+            }
+
         }
     }
-}
+
+
+
+
+
